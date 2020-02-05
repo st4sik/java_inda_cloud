753b7f3e408caa079ab4d74cf1b21d9c9c1854f4
st4sik <ryabokonsm@mail.ru>
2020-02-05 13:14:14 +0400

    Added hystrix dashboard for PO

--- po/src/main/java/org/nipu/po/PoApplication.java
+++ po/src/main/java/org/nipu/po/PoApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients(clients = ProductSpecificationRepository.class)
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class PoApplication {
 
     public static void main(String[] args) {
